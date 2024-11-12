b443603d111f720ef0957292916455a871790277
Patriott07 <pwangtampn@gmail.com>
2024-11-12 22:35:02 +0700

    selesai slicing sisa logika + responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css';
+import './output.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Datas from './pages/Datas';
+import Info from './pages/Info';
+import PantauData from './pages/PantauData';
+import UploadBerkas from './pages/UploadBerkas';
 
 function App() {
- <BrowserRouter>
-  <Routes>
-    {/* <Route path='/' element={} /> */}
-  </Routes>
- </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/datas' element={<Datas />} />
+        <Route path='/info-upload' element={<Info />} />
+        <Route path='/data/pantau' element={<PantauData />} />
+        <Route path='/upload' element={<UploadBerkas />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
