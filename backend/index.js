e2a037485943ea649112d8cf945b03c48101bc2b
sbajsksneks <137669925+sbajsksneks@users.noreply.github.com>
2024-12-08 01:04:17 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import fileupload from 'express-fileupload';
 import cors from 'cors';
+import path from 'path';
 
-// import route
+// Import route
 import DataRoute from './router/data.route.js';
 
 const app = express();
-dotenv.config({path : './.env'});
+dotenv.config({ path: './.env' });
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cors());
 app.use(fileupload());
 
-// use routes
+// Use routes
 app.use('/api/data', DataRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Handle all other routes by serving the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
