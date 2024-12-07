cf352bfdc3bd6a8a9234afe4e27f7a43def31dca
sbajsksneks <137669925+sbajsksneks@users.noreply.github.com>
2024-12-08 01:08:09 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import fileupload from 'express-fileupload';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Import route
 import DataRoute from './router/data.route.js';
@@ -17,12 +18,16 @@ app.use(fileupload());
 // Use routes
 app.use('/api/data', DataRoute);
 
+// Resolve __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Handle all other routes by serving the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
